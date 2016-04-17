912e2efea0776cbbbd27482f7b3b6dbab55dcae0
Dani <Dani@192.168.1.36>
2016-04-17 19:58:06 +0200

    Commit de dani

--- Calculadora/src/funciones/Division.java
+++ Calculadora/src/funciones/Division.java
@@ -13,7 +13,7 @@ public class Division extends Operacion {
 
     @Override
     public double doOperacion(double anterior, double posterior) {
-        return anterior/posterior;
+        return anterior+posterior;
     }
 
     @Override
